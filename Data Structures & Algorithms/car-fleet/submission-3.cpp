b8fd8b1d0121d3f81b0234b09280c4b73da8a026
hnakn <gk002005@gmail.com>
2026-05-12 22:51:07 +0530

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> v;
+        for(int i=0;i<position.size();i++)
+        {
+            v.push_back({position[i],i});
+        }
+        sort(v.begin(),v.end());
+        stack<float> stk;
+        for(int i=v.size()-1;i>=0;i--)
+        {
+            float steps = (target-v[i].first)/speed[v[i].second];
+            if(stk.empty() || stk.top()<steps) stk.push(steps);
+        }
+        return stk.size();
+    }
+};
