9d4aab6652d79605554af92e1f4efbe9b1d16180
hnakn <gk002005@gmail.com>
2026-05-14 16:46:02 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,19 @@
+class TimeMap {
+private:
+    unordered_map<string,vector<pair<int,string>>> mp;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        vector<pair<int,string>> curr = mp[key];
+        if(curr.empty()) return "";
+        sort(curr.begin(),curr.end(),greater<pair<int,string>>());
+        return curr[0].second;    
+    }
+};
