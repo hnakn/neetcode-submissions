e7727ee13680214d0baa519fcef6e80178327265
hnakn <gk002005@gmail.com>
2026-05-23 12:47:55 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> mp;
+        int l=0,r=0,maxi=0,result=0;
+
+        while(r<s.size())
+        {
+            mp[s[r]]++;
+            if(mp[s[r]]>maxi) maxi = mp[s[r]];
+            while((r-l+1-maxi>k))
+            {
+                l++;
+                mp[s[l]]--;
+            }
+            result = max(r-l+1,result);
+            r++;
+        }    
+        return result;
+    }
+};
