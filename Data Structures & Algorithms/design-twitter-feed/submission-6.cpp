1af59cdd9e9d41bbdeec976cb3914d8025281239
hnakn <gk002005@gmail.com>
2026-05-25 13:09:33 +0530

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.cpp
@@ -0,0 +1,60 @@
+class Twitter {
+private:
+    unordered_map<int,vector<int>> followers;
+    unordered_map<int,vector<pair<int,int>>> tweets;
+    int t=0;
+public:
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({t,tweetId});
+        t++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> result;
+        for(auto& i : tweets[userId])
+        {
+            pq.push(i);
+            if(pq.size()>10) pq.pop();
+        }
+
+        for(int i : followers[userId])
+        {
+            if(i==-1) continue;
+            for(auto& i : tweets[i])
+            {
+                pq.push(i);
+                if(pq.size()>10) pq.pop();
+            }
+        }
+
+        while(!pq.empty())
+        {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+        
+    }
+    
+    void follow(int followerId, int followeeId) {
+        if(followerId==followeeId) return;
+        if(followers[followerId].count(followeeId)==0) followers[followerId].push_back(followeeId);        
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(followerId==followeeId) return;
+        for(int i=0;i<followers[followerId].size();i++)
+        {
+            if(followers[followerId][i]==followeeId)
+            {
+                followers[followerId][i]=-1;
+            }
+        }
+    }
+};
