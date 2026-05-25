a057ff428448d504ae4db66043a9e360f99dcb1b
hnakn <gk002005@gmail.com>
2026-05-25 15:51:09 +0530

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    struct cmp{
+
+        bool operator() (Interval a, Interval b){
+            return a.start < b.start;
+        }
+    };
+
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp());
+        int curr = intervals[0].end;
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals[i].start<curr)
+            {
+                return false;
+            }
+            curr = max(curr,intervals[i].end);
+        }
+        return true;
+    }
+};
