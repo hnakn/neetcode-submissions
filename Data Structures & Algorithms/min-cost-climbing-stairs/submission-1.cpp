95d10a53acf7f0c76d356d3ece5adead232aa661
hnakn <gk002005@gmail.com>
2026-05-28 10:51:05 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sol(vector<int>& cost, int index,vector<int>& dp)
+    {
+        if(index<2) return 0;
+        if(dp[index]!=-1) return dp[index];
+        return min(sol(cost,index-1,dp)+cost[index-1],sol(cost,index-2,dp)+cost[index-2]);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n+1,-1);
+        return sol(cost,n,dp);
+    }
+};
