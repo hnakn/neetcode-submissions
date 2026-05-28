1955e0178c4a87ac9c1bef418bf7179c7e559276
hnakn <gk002005@gmail.com>
2026-05-28 17:34:23 +0530

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int i, int j, int m, int n)
+    {
+        if(i<0 || j<0 || i>m-1 || j>n-1) return;
+        if(board[i][j]=='X') return;
+        if(i!=0 && j!=0 && i!=m-1 && j!=n-1) board[i][j]='A';
+        if(i>0 && board[i-1][j]=='X') dfs(board,i-1,j,m,n);
+        if(j>0 && board[i][j-1]=='X') dfs(board,i,j-1,m,n);
+        if(i<m-1 && board[i+1][j]=='X') dfs(board,i+1,j,m,n);
+        if(j<n-1 && board[i][j+1]=='X') dfs(board,i,j+1,m,n);       
+    }
+    void solve(vector<vector<char>>& board) {
+        int m=board.size(),n=board[0].size();
+        for(int i=0;i<m;i++)
+        {
+            if(board[i][0]=='O')
+            {
+                dfs(board,i,0,m,n);
+            }
+            if(board[i][n-1]=='O')
+            {
+                dfs(board,i,n-1,m,n);
+            }
+        }
+
+        for(int j=0;j<n;j++)
+            {
+                if(board[0][j]=='O')
+                {
+                    dfs(board,0,j,m,n);
+                }
+                if(board[m-1][j]=='O')
+                {
+                    dfs(board,m-1,j,m,n);
+                }
+            }
+
+        for(int i=1;i<m-1;i++)
+        {
+            for(int j=1;j<n-1;j++)
+            {
+                if(board[i][j]=='A')
+                {
+                    board[i][j]='O';
+                }
+                else if(board[i][j]=='O')
+                {
+                    board[i][j]='X';
+                }
+            }
+        }
+    }
+};
