55385c9e443a6cd85540d7fa2852688c50485cae
hnakn <gk002005@gmail.com>
2026-05-29 11:23:33 +0530

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxprod=1,minprod=-1,right=0,maxi=INT_MIN;
+        while(right<nums.size())
+        {
+            if(nums[right]<0)
+            {
+                maxprod=1;          
+            }
+            else 
+            {
+                maxprod *= nums[right];
+            }
+            right++;   
+            maxi=max(maxi,max(maxprod,minprod));
+        }
+        return maxi;
+    }
+};
