6e874f575279d3f63b79aa9d2d15efefd2914aa8
hnakn <gk002005@gmail.com>
2026-06-01 11:14:51 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sol(string s, int index, vector<int>& dp)
+    {
+        if(index==s.size()) return 1;
+        if(s[index]-'0'==0) return 0;
+        if(dp[index]!=-1) return dp[index];
+        for(int i=1;i<=2;i++)
+        {
+            if(index+i>s.size()) continue;
+            string curr = s.substr(index,i);
+            if(stoi(curr)<=26 && stoi(curr)>0)
+            {
+                if(dp[index]==-1) dp[index]=sol(s,index+i,dp);
+                else dp[index]+= sol(s,index+i,dp);
+            }
+        }
+        return dp[index];
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size(),-1);
+        return sol(s,0,dp);
+    }
+};
