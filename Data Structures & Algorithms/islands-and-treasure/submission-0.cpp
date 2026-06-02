85627cb43211728eda6716bcea3b1159fdf8db15
hnakn <gk002005@gmail.com>
2026-06-02 12:29:47 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int inf = INT_MAX;
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int m = grid.size(),n=grid[0].size();
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==0) q.push({{i,j},0});
+            }
+        }
+
+        while(!q.empty())
+        {
+            int size=q.size();
+            for(int a=0;a<size;a++)
+            {
+                int i=q.front().first.first;
+                int j=q.front().first.second;
+                int level = q.front().second;
+                q.pop();
+                if(i<m-1 && grid[i+1][j]==inf) 
+                {
+                    q.push({{i+1,j},level+1});
+                    grid[i+1][j]=level+1;
+                }
+                if(i>0 && grid[i-1][j]==inf) 
+                {
+                    q.push({{i-1,j},level+1});
+                    grid[i-1][j]=level+1;
+                }
+                if(j<n-1 && grid[i][j+1]==inf) 
+                {
+                    q.push({{i,j+1},level+1});
+                    grid[i][j+1]=level+1;
+                }
+                if(j>0 && grid[i][j-1]==inf) 
+                {
+                    q.push({{i,j-1},level+1});
+                    grid[i][j-1]=level+1;
+                }
+            }
+            
+        }
+    }
+};
