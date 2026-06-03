f4498a535ea660b0ec4b8ddc1ee27c9d4448e17d
hnakn <gk002005@gmail.com>
2026-06-03 12:19:15 +0530

    Add: course-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-8.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> result;
+    bool dfs(int index, vector<int>& vis, unordered_map<int,vector<int>>& mp)
+    {
+        if(vis[index]==1) return false;
+        if(vis[index]==2) return true;
+        
+        if(mp.count(index)==0) 
+        {
+            result.push_back(index);
+            return true;
+        }
+        vis[index]=1;
+        
+        for(int i : mp[index]) if(!dfs(i, vis, mp)) return false;
+    
+        vis[index]=2;
+        mp.erase(index);
+        result.push_back(index);
+        return true;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> vis(numCourses,0);
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!dfs(i,vis, mp)) return {};
+        }
+        return result;
+    }
+};
