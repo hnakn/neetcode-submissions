2b8c1a5db93e2ba42981ab80ebb53b17bd8a3205
hnakn <gk002005@gmail.com>
2026-06-03 10:19:33 +0530

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool dfs(int index, vector<int>& vis, unordered_map<int,vector<int>>& mp)
+    {
+        if(mp.count(index)==0) return true;
+        if(vis[index]==1) return false;
+        vis[index]=1;
+        bool flag = true;
+        for(int i : mp[index]) flag=flag && dfs(i, vis, mp);
+        vis[index]=0;
+        
+        return flag;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> vis(numCourses,0);
+        bool flag = true;
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        for(auto& p : prerequisites)
+        {
+            if(vis[p[0]]) continue;
+            flag = flag && dfs(p[0],vis, mp);
+        }
+        return flag;
+    }
+};
