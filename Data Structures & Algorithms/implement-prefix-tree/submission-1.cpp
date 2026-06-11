1246acd37f67cd4ef6182da0f39b8e5cba4b74fe
hnakn <gk002005@gmail.com>
2026-06-11 15:16:11 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,46 @@
+struct TrieNode {
+    unordered_map<char,TrieNode*> mp;
+    bool end;
+    TrieNode() {
+        this->end = false;
+    }
+};
+class PrefixTree {
+public:
+    TrieNode* root;
+    PrefixTree() {
+        TrieNode* x = new TrieNode();
+        this->root = x;
+    }
+    
+    void insert(string word) {
+        TrieNode* temp = root;
+        for(char c : word)
+        {
+            if(temp->mp.count(c)==0) temp->mp[c] = new TrieNode();
+            temp = temp->mp[c];
+        }
+        temp->end = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* temp = root;
+        for(char c : word)
+        {
+            if(!temp->mp.count(c)) return false;
+            temp = temp->mp[c];
+        }
+        if(temp->end) return true;
+        return false;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* temp = root;
+        for(char c : prefix)
+        {
+            if(!temp->mp.count(c)) return false;
+            temp = temp->mp[c];
+        }
+        return true;
+    }
+};
