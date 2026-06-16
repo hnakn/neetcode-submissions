55d30f9f299ddf1834672beb0eea82daf667955f
hnakn <gk002005@gmail.com>
2026-06-16 14:39:33 +0530

    Add: reconstruct-flight-path - submission-5

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-5.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> result = {};
+    void dfs(string curr, unordered_map<string,vector<string>>& adj, int count,vector<string>& curr_result)
+    {
+        if(curr_result.size()==count+1)
+        {
+            if(result.empty() || curr_result<result) result = curr_result;
+        }
+        if(curr.size()==0) return;
+        for(int i=0;i<adj[curr].size();i++)
+        {
+            string temp = adj[curr][i];
+            if(temp.size()==0) continue;
+            curr_result.push_back(temp);
+            adj[curr][i]="";
+            dfs(temp,adj,count,curr_result);
+            curr_result.pop_back();
+        }
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string,vector<string>> adj;
+        sort(tickets.begin(),tickets.end());
+        for(auto& it : tickets)
+        {
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<string> curr_result = {"JFK"};
+        dfs("JFK",adj,tickets.size(),curr_result);        
+        return result;
+    }
+};
