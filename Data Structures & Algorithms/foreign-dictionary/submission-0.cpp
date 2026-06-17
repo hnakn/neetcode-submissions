1d2cf2666e224265e9ea012dc5cce7701e15d5a9
hnakn <gk002005@gmail.com>
2026-06-17 17:09:19 +0530

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string result;
+    bool flag = false;
+    void topo(unordered_map<char,vector<char>>& mp, vector<int>& vis, char curr)
+    {
+        if(vis[curr-'a']==1) 
+        {
+            flag=true;
+            return;
+        }
+        if(vis[curr-'a']==2) return;
+        vis[curr-'a']=1;
+        for(char c : mp[curr])
+        {
+            topo(mp,vis,c);
+        }
+        vis[curr-'a']=2;
+        result.push_back(curr);
+    }
+
+    string foreignDictionary(vector<string>& words) {
+        unordered_map<char,vector<char>> mp;
+
+        for(int i=0;i<words.size()-1;i++)
+        {
+            string a = words[i],b = words[i+1];
+            int j=0;
+            while(j<a.size() && j<b.size())
+            {
+                if(a[j]==b[j])
+                {
+                    j++;
+                    continue;
+                }
+                mp[a[j]].push_back(b[j]);
+                j=-1;
+                break;
+            }
+            if(j!=-1 && a.size()>b.size()) return "";
+        }
+
+        vector<int> vis(26,0);
+        for(auto& it : mp)
+        {
+            topo(mp,vis,it.first);
+        }
+        if(flag) return "";
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
