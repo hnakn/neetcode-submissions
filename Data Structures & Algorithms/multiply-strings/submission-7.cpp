a3c3c3cdcc927b465d894143fbbbfd812ba33368
hnakn <gk002005@gmail.com>
2026-06-19 12:17:07 +0530

    Add: multiply-strings - submission-7

---
+++ Data Structures & Algorithms/multiply-strings/submission-7.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        vector<int> result(num1.size()+num2.size(),0);
+
+        for(int i=num2.size()-1;i>=0;i--)
+        {
+            for(int j=num1.size()-1;j>=0;j--)
+            {
+                int pos = i+j+1;
+                int posnext = i+j;
+                int sum =  result[pos] + ((num2[i]-'0')*(num1[j]-'0'));
+                
+                result[pos] = sum%10;
+                result[posnext] += sum/10;
+            }            
+        }
+        string s = "";
+        int i=0;
+        while(i<result.size() && result[i]==0) i++;
+        for(;i<result.size();i++)
+        {
+            s += to_string(result[i]);
+        }
+        if(s.empty()) return "0";
+        return s;
+    }
+};
